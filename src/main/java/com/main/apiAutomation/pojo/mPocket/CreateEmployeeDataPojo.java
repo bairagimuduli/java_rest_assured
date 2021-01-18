104f454b81a2a9f69abed3b0ec6a4c2ceb1d894f
bantu <bairagi1007@live.com>
2021-01-18 11:25:07 +0530

    mpocket done

---
+++ src/main/java/com/main/apiAutomation/pojo/mPocket/CreateEmployeeDataPojo.java
@@ -0,0 +1,69 @@
+package com.main.apiAutomation.pojo.mPocket;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "name",
+        "salary",
+        "age"
+})
+public class CreateEmployeeDataPojo {
+
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("salary")
+    private String salary;
+    @JsonProperty("age")
+    private String age;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("salary")
+    public String getSalary() {
+        return salary;
+    }
+
+    @JsonProperty("salary")
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    @JsonProperty("age")
+    public String getAge() {
+        return age;
+    }
+
+    @JsonProperty("age")
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
